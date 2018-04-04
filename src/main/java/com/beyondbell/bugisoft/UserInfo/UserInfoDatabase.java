c4e27368507f648800ac708d773e3c279847662b
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-04-04 15:43:52 -0700

    Created Registration Methods

--- src/main/java/com/beyondbell/bugisoft/UserInfo/UserInfoDatabase.java
+++ src/main/java/com/beyondbell/bugisoft/UserInfo/UserInfoDatabase.java
@@ -1,7 +1,23 @@
 package com.beyondbell.bugisoft.UserInfo;
 
+import org.javacord.api.entity.message.MessageAuthor;
+
 import java.util.ArrayList;
 
-public class UserInfoDatabase {
-	private ArrayList<UserInfo> userInfos;
+class UserInfoDatabase {
+	private static ArrayList<UserInfo> users = new ArrayList<>();
+
+	static void registerUser(MessageAuthor user) {
+		users.add(new UserInfo(user));
+	}
+
+	static UserInfo findUser(MessageAuthor author) {
+		for (UserInfo user : users) {
+			if (user.getID().equals(author.getIdAsString())) {
+				return user;
+			}
+		}
+		registerUser(author);
+		return users.get(users.size() - 1);
+	}
 }
