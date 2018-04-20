e1efc1bd6d71897a088645e5f6a2b19886bdd9b5
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-04-19 19:50:10 -0700

    Logger Initial

---
+++ src/main/java/com/beyondbell/bugisoft/Logger/Logger.java
@@ -0,0 +1,4 @@
+package com.beyondbell.bugisoft.Logger;
+
+public class Logger {
+}
---
+++ src/main/java/com/beyondbell/bugisoft/Logger/LoggerQuerry.java
@@ -0,0 +1,10 @@
+package com.beyondbell.bugisoft.Logger;
+
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.event.Event;
+
+public class LoggerQuerry {
+	public Message[] getMessages(Event event, int numberOfMessages) {
+		return null;
+	}
+}
