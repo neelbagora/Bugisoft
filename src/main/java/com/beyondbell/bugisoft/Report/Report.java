9a0509b6bf6cc88d178d1fabcc2cf28cb94bca87
neelbagora <neelbagora@gmail.com>
2018-04-19 19:31:47 -0700

    Added Changes to Report

--- src/main/java/com/beyondbell/bugisoft/Report/Report.java
+++ src/main/java/com/beyondbell/bugisoft/Report/Report.java
@@ -1,49 +1,73 @@
 package com.beyondbell.bugisoft.Report;
 
 
-import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.user.User;
 import com.beyondbell.bugisoft.TextFormatters.InputFormatter;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
-
 import java.util.ArrayList;
-import java.util.Optional;
+
 
 public class Report implements MessageCreateListener {
 
     public void onMessageCreate(MessageCreateEvent event) {
         ArrayList<String> parameters = InputFormatter.stringToParameters(event);
 
-
         if(parameters.get(0).equals("!") && parameters.get(1).equals("report")) {
-            Optional<Server> x = event.getServer();
+
             //planning on using command to get amount user messages and sending them to chat
             long id = Long.parseLong(parameters.get(2));
+
+            //identifies User
+            User member = event.getServer().get().getMemberById(id).get();
+            String display = event.getServer().get().getDisplayName(member);
+
+            //number of messages
             int amount = Integer.parseInt(parameters.get(3));
+
+            //current channel;
             TextChannel channel = event.getChannel();
+
             //arranges messages into messages arrayList
-            ArrayList<String> messages = new ArrayList<String>(amount);
-            User user = ;
-            for(int i = 0; i < amount; i++) {
-                String x = channel.getMessageById(id).toString();
-                if(x == null) {
+            int count = 1;
+            Message messages = (Message) event.getChannel().getMessagesBefore(count, event.getMessageId());
+            ArrayList<Message> storage = new ArrayList<>();
+
+
+            for(int i = 0; i < Integer.MAX_VALUE; i++) {
+                if(messages == null) {
                     break;
-                } else {
-                    messages.add(x);
+                }else if(storage.size() == amount) {
+                    break;
+                }
+                 else if(messages.getAuthor().getId() != id) {
+                    count+=1;
+                    messages = (Message) event.getChannel().getMessagesBefore(count, event.getMessage());
+                } else if(storage.size() != amount){
+                    storage.add(messages);
                 }
             }
 
             EmbedBuilder embed = new EmbedBuilder();
 
-                embed.setTitle("Messages from: ")
-                embed.addField("", );
+                embed.setTitle(event.getServer().get().getDisplayName((event.getApi().getYourself()))
+                        + "Messages from: " + display);
+
                 //for loop to add fields to embed
-                for(byte i = 0; i < amount; i++) {
+                for(int i = amount; i > 0; i--) {
+                    embed.addField("", storage.get(i).getContent());
+                    //embed.addField("Time: ", event.getMessage().get)
 
                 }
+
+                embed.setAuthor(event.getMessage().getAuthor().getName());
+            event.getChannel().sendMessage(embed);
+            event.getMessage().delete(event.getMessage().getIdAsString());
         }
+
+
     }
 }
