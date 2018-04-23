389a2d64bd75604ab26d676573d685a351bb8dfc
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-04-22 22:12:08 -0700

    Batch

--- src/main/java/com/beyondbell/bugisoft/Logger/LoggerDatabase.java
+++ src/main/java/com/beyondbell/bugisoft/Logger/LoggerDatabase.java
@@ -1,55 +1,67 @@
 package com.beyondbell.bugisoft.Logger;
 
+import com.beyondbell.bugisoft.Utilities.TextFormatters.LoggerFormatter;
 import org.javacord.api.entity.message.Message;
-import org.javacord.api.event.Event;
+import org.javacord.api.event.message.MessageCreateEvent;
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class LoggerDatabase {
-
-
-
-/*
-	public static Message[] getMessages(Event event, int numberOfMessages) throws IOException {
-		String log = readFile(fileName,numberOfMessages);
-		String[] messages = new String[numberOfMessages];
-		int index = log.indexOf("/n");
-		int initialIndex = 0;
-
-		for(int i = 0; i < messages.length; i++) {
-			if(index != -1) {
-				messages[i] = log.substring(initialIndex, index);
-				initialIndex += messages[i].length() + 2;
-				index = log.indexOf("/n", index + 1);
-			} else {
-				messages[i] = log.substring(initialIndex, log.length());
-				break;
-			}
+	public static Message[] getMessages(MessageCreateEvent event, int... messagesParameters) {
+		int numberOfMessages;
+		int startingMessage;
+		if (messagesParameters.length == 1) {
+			numberOfMessages = messagesParameters[0];
+			startingMessage = 0;
+		} else if (messagesParameters.length == 2) {
+			numberOfMessages = messagesParameters[0];
+			startingMessage = messagesParameters[1];
+		} else {
+			// TODO Catch Too Many Parameters
+			return null;
 		}
 
-		//need to convert strings to messages
-		Message[] x = new Message[2];
-		return x;
-	}
+		String path;
+		if (event.getServer().isPresent()) {
+			path = "Logs/" + event.getServer().get().getIdAsString() + "/" + event.getChannel().getIdAsString();
+		} else {
+			// TODO Not a Server Catch!!!
+			return null; // Not Good
+		}
 
-	public static String readFile(String file, int number) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(file));
+		Object[] objects = null;
 		try {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+			objects = bufferedReader.lines().toArray();
 
-			for(int i = 0; i < number; i++) {
-				if(line != null) {
-					sb.append(line);
-					sb.append("/n");
-					line = br.readLine();
-				}
-			}
-			return sb.toString();
-		} finally {
-			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+
+		if (objects == null) {
+			return null;
+			// TODO Catch
+		}
+
+		String[] logMessages = (String[]) objects;
+		ArrayList<String> messageIDs = new ArrayList<>();
+		for (int i = logMessages.length - 1 - startingMessage; i > logMessages.length - 1 - startingMessage - numberOfMessages; i--) {
+			messageIDs.add(LoggerFormatter.getIDFromLogMessage(logMessages[i]));
+		}
+
+		Message[] messages = new Message[numberOfMessages];
+		for (int i = 0; i < messages.length; i++) {
+			try {
+				messages[i] = event.getChannel().getMessageById(messageIDs.get(i)).get();
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return messages;
 	}
-	*/
 }
