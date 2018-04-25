2f4d127763e5908c60e4d7bafa12eec3195d6ee6
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-04-25 10:34:05 -0700

    Updated Rainbow Six Siege Operators

---
+++ src/main/java/com/beyondbell/bugisoft/Utilities/RainbowSixSiege/AtackingOperators.java
@@ -0,0 +1,26 @@
+package com.beyondbell.bugisoft.Utilities.RainbowSixSiege;
+
+public enum AtackingOperators {
+	// Year One
+	Ash, Thermite,
+	Twitch, Montagne,
+	IQ, Blitz,
+	Glaz, Fuze,
+	Sledge, Tatcher,
+
+	// Year One
+	Buck,
+	Blackbeard,
+	Capitao,
+	Hibana,
+
+	// Year Two
+	Jackal,
+	// Operation Health
+	Zofia,
+	Dokaebi,
+
+
+	// Year Three
+	Lion, Finka,
+}
---
+++ src/main/java/com/beyondbell/bugisoft/Utilities/RainbowSixSiege/DefendingOperators.java
@@ -0,0 +1,26 @@
+package com.beyondbell.bugisoft.Utilities.RainbowSixSiege;
+
+public enum DefendingOperators {
+	// Year One
+	Castle, Pulse,
+	Rook, Doc,
+	Jager, Bandit,
+	Tachanka, Kapkan,
+	Smoke, Mute,
+
+	// Year One
+	Frost,
+	Valkyrie,
+	Caveria,
+	Echo,
+
+	// Year Two
+	Mira,
+	// Operation Health
+	Ela,
+	Vigil,
+
+
+	// Year Three
+	// No Season One Defending Operators
+}
