01ed42682bf0c174682b5422588879ecb980b5c1
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-04-24 19:34:27 -0700

    Major Rehaul

--- src/main/java/com/beyondbell/bugisoft/Tournament/Tournament.java
+++ src/main/java/com/beyondbell/bugisoft/Tournament/Tournament.java
@@ -1,34 +1,51 @@
 package com.beyondbell.bugisoft.Tournament;
 
-import com.beyondbell.bugisoft.Utilities.TextFormatters.InputFormatter;
-import org.javacord.api.entity.user.User;
-import org.javacord.api.event.Event;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 abstract class Tournament {
+	private final String name;
+	private final TournamentType tournamentType;
+
 	private ArrayList<Player> players;
 	private ArrayList<Match> matches;
 
-	public void OnMessageCreate(MessageCreateEvent event) {
-		final String[] parameters = InputFormatter.stringToParameters(event.getMessage().getReadableContent());
-		if(parameters[0].equals("!") && parameters[1].equals("addplayer")) {
-			if(event.getServer().get().getMembersByName(parameters[3]) != null) {
-				players.add(new Player(event.getServer().get().getDisplayName
-						((event.getServer().get()
-								.getDisplayName((User) event.getServer().get().getMembersByName(parameters[3])));
-			} else {
-				event.getChannel().sendMessage("Invalid name, check for spelling");
-			}
-		}
-		if(parameters[0].equals("!") && parameters[1].equals("setMatchSize") {
-			players.size() / 
-		}
+	public Tournament(TournamentType tournamentType, String name) {
+
+		this.tournamentType = tournamentType;
+		this.name = name;
 	}
 
 	public void generateBracketLayer() {
 		//TODO
+	}
+
+	public void printMatchDetails(MessageCreateEvent event, int matchIndex) {
+		// Creates Main Details
+		EmbedBuilder gameDetails = new EmbedBuilder()
+				.setTitle("Match: " + matchIndex + " In Tournament: " + name + " Details")
+				.addField("Match Status", matches.get(matchIndex).getMatchPhase().name(), false)
+				.addField("Blue Team Players", matches.get(matchIndex).getBlueTeamPrintable(), true)
+				.addField("Orange Team Players", matches.get(matchIndex).getOrangeTeamPrintable(), true)
+				.addField("Maps Played", matches.get(matchIndex).getMapsPlayedPrintable(), false);
+				// TODO Add Banned Operators
+
+		// Checks for Team Victory
+		if (matches.get(matchIndex).getMatchPhase() == Match.MatchPhase.Finished) {
+			if (matches.get(matchIndex).getBlueWon()) {
+				gameDetails.setColor(Color.BLUE).setFooter("Blue Team Won!");
+			} else {
+				gameDetails.setColor(Color.ORANGE).setFooter("Orange Team Won!");
+			}
+		} else {
+			gameDetails.setFooter("Game Results Pending...");
+		}
+
+		// Prints the Game Details
+		event.getChannel().sendMessage(gameDetails);
 	}
 
 	public enum TournamentType {
