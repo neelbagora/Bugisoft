1bf0eb204d03dad742e31b459519fe9d874f4d62
neelbagora <neelbagora@gmail.com>
2018-05-12 18:52:27 -0700

    new Profanity Filter

---
+++ src/main/java/com/beyondbell/bugisoft/Utilities/MessageUtilities/ProfanityFilter.java
@@ -0,0 +1,34 @@
+package com.beyondbell.bugisoft.Utilities.MessageUtilities;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import java.io.*;
+
+public class ProfanityFilter {
+    private final String message;
+
+
+    public ProfanityFilter(MessageReceivedEvent event)  {
+        message = event.getMessage().toString().toLowerCase();
+
+        FileInputStream BadWords = null;
+        try {
+            BadWords = new FileInputStream("BadWords");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(BadWords));
+
+        try {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                if(message.contains(line)) {
+                    event.getMessage().delete().queue();
+                    break;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
