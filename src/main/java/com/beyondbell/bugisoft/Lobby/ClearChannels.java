232506f35a489fac251751506819937b85a3fccb
neelbagora <neelbagora@gmail.com>
2018-05-14 19:05:21 -0700

    ClearChannels now built to accomodate gameCategory setting

--- src/main/java/com/beyondbell/bugisoft/Lobby/ClearChannels.java
+++ src/main/java/com/beyondbell/bugisoft/Lobby/ClearChannels.java
@@ -1,12 +1,14 @@
 package com.beyondbell.bugisoft.Lobby;
 
+import com.beyondbell.bugisoft.Bot;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.core.entities.Category;
 
 public class ClearChannels {
 	public ClearChannels(GuildVoiceLeaveEvent event) {
+        String categoryString = Bot.settings.getProperty("gameChannelsCategory");
 		for(Category category : event.getGuild().getCategories()) {
-		    if(category.getName().toLowerCase().equals("temporary channels") && category.getVoiceChannels().contains(event.getChannelLeft())
+		    if(category.getName().equals(categoryString) && category.getVoiceChannels().contains(event.getChannelLeft())
                     && !event.getChannelLeft().getName().toLowerCase().equals("lobby")) {
 		        event.getChannelLeft().delete().queue();
             }
