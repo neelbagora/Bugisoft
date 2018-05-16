6e70fc564fbcb4a108d2549d0ccb11f78ff0bf51
neelbagora <neelbagora@gmail.com>
2018-05-15 19:31:39 -0700

    Error Fixes

--- src/main/java/com/beyondbell/bugisoft/Lobby/ClearChannels.java
+++ src/main/java/com/beyondbell/bugisoft/Lobby/ClearChannels.java
@@ -11,6 +11,7 @@ public class ClearChannels {
     GuildVoiceLeaveEvent event;
     Category category;
     VoiceChannel lobby;
+    String categoryString;
 
 	public ClearChannels(final GuildVoiceLeaveEvent event) {
         try {
@@ -20,18 +21,29 @@ public class ClearChannels {
             return;
         }
 	    this.event = event;
-        String categoryString = Bot.settings.getProperty("gameChannelsCategory").toLowerCase();
-        category = event.getGuild().getCategoriesByName(categoryString, true).get(0);
+        categoryString = Bot.settings.getProperty("gameChannelsCategory").toLowerCase();
+        if(event.getGuild().getCategoriesByName(categoryString,true).size() > 0) {
+            category = event.getGuild().getCategoriesByName(categoryString, true).get(0);
+        }
+
 
 	}
 
     public void clearEmpty() {
             for(int i = 0; i < event.getGuild().getVoiceChannels().size(); i++) {
-                if (!lobby.equals(event.getGuild().getVoiceChannels().get(i))
-                        && event.getGuild().getVoiceChannels().get(i).getParent().equals(category)
-                        && event.getGuild().getVoiceChannels().get(i).getMembers().size() == 0) {
-                    new GuildController(event.getGuild()).getGuild().getVoiceChannels().get(i).delete().queue();
+                if(event.getGuild().getCategoriesByName(categoryString, true).size() > 0) {
+                    if (!lobby.equals(event.getGuild().getVoiceChannels().get(i))
+                            && event.getGuild().getVoiceChannels().get(i).getParent().equals(category)
+                            && event.getGuild().getVoiceChannels().get(i).getMembers().size() == 0) {
+                        new GuildController(event.getGuild()).getGuild().getVoiceChannels().get(i).delete().queue();
+                    }
+                } else {
+                    if (!lobby.equals(event.getGuild().getVoiceChannels().get(i))
+                            && event.getGuild().getVoiceChannels().get(i).getMembers().size() == 0) {
+                        new GuildController(event.getGuild()).getGuild().getVoiceChannels().get(i).delete().queue();
+                    }
                 }
+
             }
 
 
