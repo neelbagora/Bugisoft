d2f5c5f258c79c135107e942faed3072050301b1
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-08-07 01:06:29 -0700

    Added Step Comments

--- src/main/java/com/beyondbell/bugisoft/Bugisoft.kt
+++ src/main/java/com/beyondbell/bugisoft/Bugisoft.kt
@@ -3,5 +3,12 @@ package com.beyondbell.bugisoft
 object Bot {
     @JvmStatic
     fun main(args: Array<String>) {
+        // Reads Tokens
+        // Checks if Help was Requested
+        // Checks if Version was Requested
+        // Checks for Update
+        // Loads Settings
+        // Loads Token
+        // Loads Bot
     }
 }
