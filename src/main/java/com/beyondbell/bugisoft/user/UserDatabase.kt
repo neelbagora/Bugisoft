1822cbf03100e114b0e1d2c8d345c244b97441cf
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-08-15 08:31:21 -0700

    Added Getter
    Made method synchronized as to prevent duplicate creations in case of a user spamming user related commands.

--- src/main/java/com/beyondbell/bugisoft/user/UserDatabase.kt
+++ src/main/java/com/beyondbell/bugisoft/user/UserDatabase.kt
@@ -2,4 +2,14 @@ package com.beyondbell.bugisoft.user
 
 object UserDatabase {
     private val users = HashMap<Long, BugisoftUser>()
+
+    @Synchronized
+    public fun getUser(id: Long) : BugisoftUser {
+        return if (users.containsKey(id)) {
+            users[id]!!
+        } else {
+            users[id] = BugisoftUser(id)
+            users[id]!!
+        }
+    }
 }
