11ab8f9aff6a902c87f2b60c097a3541fa73c2fd
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-08-16 21:10:47 -0700

    Added Comments

--- src/main/java/com/beyondbell/bugisoft/Constants.kt
+++ src/main/java/com/beyondbell/bugisoft/Constants.kt
@@ -1,12 +1,17 @@
 package com.beyondbell.bugisoft
 
 import java.util.concurrent.TimeUnit
+
+// Bugisoft.kt
 const val DEFAULT_SETTINGS_FILE_NAME = "settings"
 const val DEFAULT_TOKEN_FILE_NAME = "token"
 
+// ErrorMessages.kt
 const val DEFAULT_MESSAGE_TIMEOUT = 5
 val DEFAULT_TIME_UNIT = TimeUnit.SECONDS
 
+// Invite.kt
 const val DEFAULT_INVITE_TIME = 10
 
+// Ping.kt
 const val MAX_CONCURRENT_PING_REQUESTS = 2
