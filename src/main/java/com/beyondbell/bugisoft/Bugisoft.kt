df0cbb168adde16b69cbeff690f760da2979c115
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-12-04 12:01:42 -0800

    Random Updates

--- src/main/java/com/beyondbell/bugisoft/Bugisoft.kt
+++ src/main/java/com/beyondbell/bugisoft/Bugisoft.kt
@@ -1,143 +1,151 @@
 package com.beyondbell.bugisoft
 
 import com.beyondbell.bugisoft.eventHandling.EventHandler
-import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory
-import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDABuilder
 import net.dv8tion.jda.core.OnlineStatus
 import net.dv8tion.jda.core.entities.Game
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.IOException
+import java.util.EnumMap
 import java.util.Properties
 import javax.security.auth.login.LoginException
 
 val LOGGER: Logger = LogManager.getRootLogger()
 val SETTINGS = Properties()
 
-fun main(args: Array<String>) {
-	// Reads Tokens
-	val tokenValues = try {
-		readTokens(args)
+fun main(vararg args: String) {
+	val bugisoft = Bugisoft(args)
+	SETTINGS.putAll(bugisoft.loadSettings())
+	bugisoft.start()
+}
+
+private class Bugisoft(args: Array<out String>) {
+	private val arguments = try {
+		getTokensFromArgs(args)
 	} catch (e: IllegalArgumentException) {
-		// Displays Help if Tokens are Wrong
-		displayHelp()
-		return
+		displayHelp(true)
+		EnumMap<Token, String>(Token::class.java)
 	}
 
-	// Checks if Help was Requested
-	if (tokenValues.containsKey(Token.Help)) {
-		displayHelp()
-		return
-	}
+	private var canRun = true
 
-	// Checks if Version was Requested
-	if (tokenValues.containsKey(Token.Version)) {
-		displayVersion()
-		return
-	}
-
-	// Checks for Update
-	if (!tokenValues.containsKey(Token.NoUpdate)) {
-		if (hasUpdate()) {
-			update()
-			return
+	init {
+		if (arguments.contains(Token.Help)) {
+			displayHelp(false)
 		}
 	}
 
-	// Loads Settings
-	loadSettings(tokenValues.getOrDefault(Token.SettingsFileLocation, DEFAULT_SETTINGS_FILE_NAME))
+	fun start() {
+		// Loads Token
+		val botProperties = try {
+			readToken(tokenValues.getOrDefault(Token.TokenFile, DEFAULT_TOKEN_FILE_NAME))
+		} catch (e: IOException) {
+			LOGGER.fatal("Cannot Find Token File! Creating Empty Token File. Please Populate It.")
+			createEmptyTokenFile(tokenValues.getOrDefault(Token.TokenFile, DEFAULT_TOKEN_FILE_NAME))
+			return
+		}
 
-	// Loads Token
-	val botProperties = try {
-		readToken(tokenValues.getOrDefault(Token.TokenFile, DEFAULT_TOKEN_FILE_NAME))
-	} catch (e: IOException) {
-		LOGGER.fatal("Cannot Find Token File! Creating Empty Token File. Please Populate It.")
-		createEmptyTokenFile(tokenValues.getOrDefault(Token.TokenFile, DEFAULT_TOKEN_FILE_NAME))
-		return
+
+		if (canRun) {
+			try {
+				JDABuilder()
+						.addEventListener(EventHandler)
+						.setAudioEnabled(true)
+						.setBulkDeleteSplittingEnabled(false)
+						.setAutoReconnect(true)
+						.setMaxReconnectDelay(32)
+						.setRequestTimeoutRetry(true)
+						.setCompressionEnabled(false)
+						.setContextEnabled(false)
+						.setCorePoolSize(1)
+						.setEnableShutdownHook(true)
+						.setGame(Game.playing("as a Sleeping Guard!"))
+						.setStatus(OnlineStatus.IDLE)
+						.build()
+			} catch (e: LoginException) {
+				LOGGER.fatal("Please Place the Correct Token Inside of the Bot Properties File")
+			}
+		}
 	}
 
-	// Loads Bot
-	try {
-		JDABuilder(AccountType.BOT)
-				.setToken(botProperties.getProperty("token"))
-				.addEventListener(EventHandler)
-				.setAudioEnabled(true)
-				.setAudioSendFactory(NativeAudioSendFactory())
-				.setBulkDeleteSplittingEnabled(false)
-				.setAutoReconnect(true)
-				.setMaxReconnectDelay(32)
-				.setRequestTimeoutRetry(true)
-				.setCompressionEnabled(false)
-				.setContextEnabled(false)
-				.setCorePoolSize(1)
-				.setEnableShutdownHook(true)
-				.setGame(Game.playing("as a Sleeping Guard!"))
-				.setStatus(OnlineStatus.IDLE)
-				.build()
-	} catch (e: LoginException) {
-		LOGGER.fatal("Please Place the Correct Token Inside of the Bot Properties File")
+	fun loadSettings() : Properties {
+		val settings = Properties()
+		if () {
+
+		}
 	}
-}
 
-private fun readTokens(args: Array<String>): HashMap<Token, String> {
-	val tokens = HashMap<Token, String>()
-
-	//throw IllegalArgumentException()
-
-	return tokens
-}
-
-private fun displayHelp() {
-	TODO("Display Arguments Help")
-}
-
-private fun displayVersion() {
-	TODO("Display the Bot's Version")
-}
-
-private fun hasUpdate(): Boolean {
-	return false
-}
-
-private fun update() {
-	TODO("Update the Bot")
-}
-
-private fun loadSettings(file: String) {
-	try {
-		val botSettingsFile = FileInputStream(file)
-		SETTINGS.load(botSettingsFile)
-		botSettingsFile.close()
-	} catch (e: IOException) {
-		LOGGER.warn("Cannot Find Settings File! Creating a Blank One!")
-		createNewSettingsFile(file)
-		loadSettings(file)
+	private fun getTokensFromArgs(args: Array<out String>): EnumMap<Token, String> {
+		val tokens = EnumMap<Token, String>(Token::class.java)
+		for (i in 0 until args.size) {
+			if (args[i].startsWith('-')) {
+				for (token in Token.values()) {
+					for (tokenRepresentation in token.tokenRepresentations) {
+						if (args[i] == tokenRepresentation) {
+							tokens.put(token, )
+							break
+						}
+					}
+				}
+				throw IllegalArgumentException
+			}
+		}
+		for (arg in args) {
+		}
+		return tokens
 	}
-}
 
-private fun createNewSettingsFile(file: String) {
-	TODO("Create a New Settings File")
-}
-
-private fun readToken(tokenFile: String): Properties {
-	val botProperties = Properties()
-	try {
-		val botPropertiesFile = FileInputStream(tokenFile)
-		botProperties.load(botPropertiesFile)
-		botPropertiesFile.close()
-	} catch (e: IOException) {
-		throw e
+	private fun readToken(tokenFile: String): Properties {
+		val botProperties = Properties()
+		try {
+			val botPropertiesFile = FileInputStream(tokenFile)
+			botProperties.load(botPropertiesFile)
+			botPropertiesFile.close()
+		} catch (e: IOException) {
+			createEmptyTokenFile(tokenFile)
+		}
+		return botProperties
 	}
-	return botProperties
-}
 
-private fun createEmptyTokenFile(file: String) {
-	TODO("Create an Empty Token File")
-}
+	internal fun loadSettingsFile(file: String) {
+		try {
+			val botSettingsFile = FileInputStream(file)
+			SETTINGS.load(botSettingsFile)
+			botSettingsFile.close()
+		} catch (e: IOException) {
+			LOGGER.warn("Cannot Find Settings File! Creating a Blank One!")
+			createNewSettingsFile(file)
+			loadSettings(file)
+		}
+	}
 
-enum class Token {
-	Help, Version, NoUpdate,
-	TokenFile, SettingsFileLocation
+	private fun createNewSettingsFile(file: String) {
+		TODO("Create a New Settings File")
+	}
+
+	private fun createEmptyTokenFile(file: String) {
+		val botProperties = Properties()
+		botProperties.put("token", "")
+		val botTokenFile = FileOutputStream(file)
+		botTokenFile.write()
+	}
+
+	private fun displayHelp(forced: Boolean) {
+		canRun = false
+
+		val helpMessageBuilder = StringBuilder()
+		if (forced) {
+			LOGGER.info(helpMessageBuilder.toString())
+		} else {
+			LOGGER.fatal(helpMessageBuilder.toString())
+		}
+	}
+
+	private enum class Token(vararg val tokenRepresentations: String) {
+		Help("-help", "-h"),
+		TokenFileLocation("-t", "-token"), SettingsFileLocation("-s", "-settings")
+	}
 }
